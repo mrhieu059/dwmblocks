ebc965b7f1814d7c5021b7ec9606a3150debd4cd
mrhieu059 <mrhieu059@gmail.com>
2020-05-16 10:54:13 -0400

    Added ssid

--- blocks.h
+++ blocks.h
@@ -1,7 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{" ", "song_Info",						5,		0},
 	{" ", "vpn",						5,		0},
+	{" ", "ssid",						5,		0},
 	{" ", "volume",						0,		10},
 	{" ", "light.sh",						0,		10},
 	{" ", "battery",						5,		0},
