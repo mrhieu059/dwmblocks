1f01efcc157da9280fa803fc8434e0342d874fc7
mrhieu059 <mrhieu059@gmail.com>
2020-06-08 06:50:41 -0400

    minor edit

--- blocks.h
+++ blocks.h
@@ -1,13 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "song_Info",						5,		0},
-	{" ", "vpn",						5,		0},
-	{" ", "/home/cinux/.local/bin/torupdo.sh",						3,		0},
-	{" ", "ssid",						5,		0},
-	{" ", "volume",						0,		10},
-	{" ", "light.sh",						0,		10},
-	{" ", "battery",						5,		0},
+	{" ", "$HOME/.local/bin/vpn_status.sh",						5,		0},
+	{" ", "$HOME/.local/bin/torupdo.sh",						5,		0},
+	{" ", "$HOME/.local/bin/ssid.sh",						5,		0},
+	{" ", "$HOME/.local/bin/volume.sh",						0,		10},
+	{" ", "$HOME/.local/bin/light.sh",						0,		10},
+	{" ", "$HOME/.local/bin/battery.sh",						5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
