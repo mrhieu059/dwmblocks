703cfa163c5a4ca3233cb049817de92dd12a61e5
mrhieu059 <mrhieu059@gmail.com>
2020-08-05 12:40:27 -0400

    Last update from gentoo

--- blocks.h
+++ blocks.h
@@ -1,12 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "$HOME/.local/bin/vpn_status.sh",						5,		0},
-	{" ", "$HOME/.local/bin/torupdo.sh",						5,		0},
-	{" ", "$HOME/.local/bin/ssid.sh",						5,		0},
-	{" ", "$HOME/.local/bin/volume.sh",						0,		10},
-	{" ", "$HOME/.local/bin/light.sh",						0,		10},
-	{" ", "$HOME/.local/bin/battery.sh",						5,		0},
+	{" ", "$HOME/.local/dot/myscript/redshift_status.sh",					360,		0},
+	{" ", "$HOME/.local/dot/myscript/sync_status.sh",					360,		0},
+	{" ", "$HOME/.local/dot/myscript/docker_status.sh",					360,		0},
+	{" ", "$HOME/.local/dot/myscript/ssid.sh",						15,		0},
+	{" ", "$HOME/.local/dot/myscript/soundcard.sh",						0,		12},
+	{" ", "$HOME/.local/dot/myscript/light.sh",						0,		10},
+	{" ", "$HOME/.local/dot/myscript/battery.sh",						5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
